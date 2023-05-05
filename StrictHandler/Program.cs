741842d9e57f6a79e3e2a2854f5eeee630e369b2
Stan Goldin <salty87@gmail.com>
2023-05-05 09:19:29 +0300

    add project

---
+++ StrictHandler/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine("Hello, Please use: var result = new StrictCalculator(mode, loose);");
---
+++ StrictHandler/StrictCalculator.cs
@@ -0,0 +1,13 @@
+﻿namespace StrictHandler
+{
+    public class StrictCalculator
+    {
+        public StrictCalculator(Modes mode, bool loose)
+        {
+            var intValue = (int)mode;
+            Result = intValue < 2 ? Convert.ToBoolean(intValue) : loose;
+        }
+        public enum Modes { StrictDisabled = 0, StrictEnabled = 1, Loose = 2 }
+        public bool Result { get; }
+    }
+}
